a475f976ceebc85c938135ee20059d6ee174b1ff
CHBaker <charlesprobaker@mail.com>
2018-03-02 11:41:11 -0700

    FEATURE: basic state routing

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
 import * as ContactsApi from './utils/ContactsAPI'
 
 class App extends Component {
     state = {
+        screen: 'list', // list & create
         contacts: []
     }
 
@@ -24,7 +26,18 @@ class App extends Component {
     render() {
         return (
             <div>
-                <ListContacts onDeleteContact={this.removeCointact} contacts={ this.state.contacts } />
+                {this.state.screen === 'list' && (
+                    <ListContacts
+                        onDeleteContact={this.removeCointact}
+                        contacts={ this.state.contacts }
+                        onNavigate={() => {
+                            this.setState({ screen: 'create' })
+                        }}
+                    />
+                )}
+                {this.state.screen === 'create' && (
+                    <CreateContact/>
+                )}
             </div>
         )
     }
--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -42,6 +42,11 @@ class ListContacts extends Component {
                         value={query}
                         onChange={(event) => this.updateQuery(event.target.value)}
                     />
+                    <a
+                        href='#create'
+                        onClick={this.props.onNavigate}
+                        className='add-contact'
+                    >Add Contact</a>
                 </div>
                 <ol className='contact-list'>
                     {showingContacts.map((contact) => (
